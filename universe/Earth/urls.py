094f34a2cbd1e4e37b34a8d75f55de61e471b31c
Abhin <abhinrajmnau@gmail.com>
2023-08-22 18:08:17 +0530

    to add

---
+++ universe/Earth/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('Earth/', views.Earth, name='Earth'),
+
+    
+]
+
+
+
+    
+
+
---
+++ universe/Earth/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def Earth(request):
+
+    return HttpResponse("hello world")
+# def home(request):
+#     return render (request,'first.html')
+# Create your views here.
+
+
